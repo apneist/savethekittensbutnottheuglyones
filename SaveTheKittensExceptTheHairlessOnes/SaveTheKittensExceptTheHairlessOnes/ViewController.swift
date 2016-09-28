407fd2c59427f4266f65ad3d4811b2ab696c9c70
apneist <apneist@gmail.com>
2016-09-28 01:23:12 +0100

    made the label work
    powa

--- SaveTheKittensExceptTheHairlessOnes/SaveTheKittensExceptTheHairlessOnes/ViewController.swift
+++ SaveTheKittensExceptTheHairlessOnes/SaveTheKittensExceptTheHairlessOnes/ViewController.swift
@@ -9,13 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet var scrollView: UIScrollView!
+        var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print ("changing this to test GIT")
+        var contentWidth: CGFloat = 0.0
         
-        // Do any additional setup after loading the view, typically from a nib.
+            for x in 0...2 {
+        
+                let image = UIImage(named: "icon\(x).jpg")
+                let imageView = UIImageView(image: image)
+                images.append(imageView)
+        
+      
+        
+        var newX: CGFloat = 0.0
+        
+        newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+                
+        scrollView.addSubview(imageView)
+                
+        imageView.frame = CGRect(x: newX, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            
+        
+        resultLabel.text = "This has \(images.count) pictures"
+                
+            
+                  }
+        
     }
 
     override func didReceiveMemoryWarning() {
