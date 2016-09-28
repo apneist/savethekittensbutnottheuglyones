a5778f3fd4c9e838d46c7ac26a720094d99fe8a6
apneist <apneist@gmail.com>
2016-09-28 02:23:01 +0100

    made it work, need to understand scrollview width

--- SaveTheKittensExceptTheHairlessOnes/SaveTheKittensExceptTheHairlessOnes/ViewController.swift
+++ SaveTheKittensExceptTheHairlessOnes/SaveTheKittensExceptTheHairlessOnes/ViewController.swift
@@ -18,29 +18,49 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
         var contentWidth: CGFloat = 0.0
         
-            for x in 0...2 {
         
-                let image = UIImage(named: "icon\(x).jpg")
-                let imageView = UIImageView(image: image)
-                images.append(imageView)
-        
-      
-        
-        var newX: CGFloat = 0.0
-        
-        newX = view.frame.midX + view.frame.size.width * CGFloat(x)
-                
-        scrollView.addSubview(imageView)
-                
-        imageView.frame = CGRect(x: newX, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+        for x in 0...2 {
             
-        
-        resultLabel.text = "This has \(images.count) pictures"
-                
+            let image = UIImage(named: "icon\(x).jpg")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
             
-                  }
+            
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollView.frame.size.width / 2 + scrollView.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+            
+            
+            resultLabel.text = "This has \(images.count) pictures"
+            
+            
+            print ("SCROLLVIEWWWWWWWWWWWW\(scrollView.frame.size.width)")
+            
+            
+        }
+        
+        
+        
+        scrollView.contentSize = CGSize(width: contentWidth - 300, height: scrollView.frame.size.height)
+        
+        scrollView.clipsToBounds = false
+        
         
     }
 
